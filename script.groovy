1194639c374cc40c30cfb85f9a390ac6e553b022
julieantonyk <99205227+julieantonyk@users.noreply.github.com>
2024-04-09 17:39:12 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {  
             stage ('Git-Checkout') {  
                 steps{
-                    git credentialsId: 'e6757c46-0750-4ab1-8cd4-c569efcbbdb1', url: 'https://github.com/sumyak/CI-CD-Pipeline.git'
+                    git credentialsId: 'e6757c46-0750-4ab1-8cd4-c569efcbbdb1', url: 'https://github.com/julieantonyk/1.git'
                     echo "Checkout successful";
                 } 
             }
